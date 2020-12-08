592a30b34a7cccc062fbb8a79137e4a9642a4db6
Ben Merken <ben.merken@gmail.com>
2020-12-08 02:35:22 +0100

    finish day 7 part 2

--- day7/solution.js
+++ day7/solution.js
@@ -8,22 +8,29 @@ const lines = fs
   .split('\r\n');
 
 const solution1 = () => {
-    let bagsContainingMyBag = lines
+  let bagsContainingMyBag = lines
     .map((line) => line.match(/^.+shiny gold bag/))
     .filter((line) => line)
     .map((line) => line.input)
     .map((line) => line.substring(0, line.match(/ bags/).index));
-    let newBagsFoundCount = bagsContainingMyBag.length;
-    let newBagsFound = true;
+  let newBagsFoundCount = bagsContainingMyBag.length;
+  let newBagsFound = true;
 
   while (newBagsFound) {
     const newBagsFoundCountPrevious = newBagsFoundCount;
 
     bagsContainingMyBag = lines
-    .map(line => line.match(`.+(${bagsContainingMyBag.reduce((acc, curr) => `${acc}|${curr}`)+ '|shiny gold'})`))
-    .filter((line) => line)
-    .map((line) => line.input)
-    .map((line) => line.substring(0, line.match(/ bags/).index));
+      .map((line) =>
+        line.match(
+          `.+(${
+            bagsContainingMyBag.reduce((acc, curr) => `${acc}|${curr}`) +
+            '|shiny gold'
+          })`
+        )
+      )
+      .filter((line) => line)
+      .map((line) => line.input)
+      .map((line) => line.substring(0, line.match(/ bags/).index));
 
     newBagsFoundCount = bagsContainingMyBag.length;
     newBagsFoundCount === newBagsFoundCountPrevious && (newBagsFound = false);
@@ -32,8 +39,37 @@ const solution1 = () => {
   console.log('Solution 1: ', newBagsFoundCount);
 };
 
-const solution2 = () => {
+const bagMap = new Map();
 
+lines.forEach((line) => {
+  const [currentBag, bagsInCurrentBag] = line.split(' bags contain ');
+
+  bagsInCurrentBag
+    .replace(/\./, '')
+    .split(', ')
+    .map((bagInCurrentBag) => {
+      const { groups } = /((?<count>\d+) )?(?<type>.*)/.exec(
+        bagInCurrentBag.replace(/ bags?/, '')
+      );
+      !bagMap.has(currentBag) && bagMap.set(currentBag, []);
+      !groups.count && (groups.count = 0);
+      bagMap.set(currentBag, [...bagMap.get(currentBag), groups]);
+    });
+});
+
+const getSumBags = (upperBag) => {
+  if (upperBag.count === 0) return 0;
+
+  const bagsInUpperBag = bagMap.get(upperBag.type);
+  let sum = 1;
+  bagsInUpperBag.forEach((bag) => (sum += bag.count * getSumBags(bag)));
+
+  return sum;
+};
+
+const solution2 = () => {
+  console.log('Solution 2: ', getSumBags({ count: 1, type: 'shiny gold' }) - 1);
 };
 
 solution1();
+solution2();
