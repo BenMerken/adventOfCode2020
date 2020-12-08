9ef866a95ed0ba19336adb16112bc0bbdaa29e6e
Ben Merken <ben.merken@gmail.com>
2020-12-09 00:48:27 +0100

    Finish day 8, part 2

--- day8/solution.js
+++ day8/solution.js
@@ -23,7 +23,44 @@ const solution1 = () => {
     operation === 'nop' && (nextInstructionIndex += 1);
   }
 
-  console.log('Solution 1, ', acc);
+  console.log('Solution 1: ', acc);
+
+  return { acc, ranInstructionsIndices };
 };
 
-solution1();
+const solution = () => {
+  const { ranInstructionsIndices } = solution1();
+  let newRanInstructionsIndices;
+  let newInstructions;
+  let nextInstructionIndex = 0;
+  let acc = 0;
+  let ranInstructionIndex = 0;
+
+  while (nextInstructionIndex < instructions.length) {
+    newRanInstructionsIndices = [];
+    newInstructions = [...instructions];
+    nextInstructionIndex = 0;
+    acc = 0;
+
+    const instructionToChangeIndex = ranInstructionsIndices[ranInstructionIndex];
+    ranInstructionIndex += 1;
+    newInstructions[instructionToChangeIndex].includes('nop') && (newInstructions[instructionToChangeIndex] = newInstructions[instructionToChangeIndex].replace('nop', 'jmp'));
+    newInstructions[instructionToChangeIndex].includes('jmp') && (newInstructions[instructionToChangeIndex] = newInstructions[instructionToChangeIndex].replace('jmp', 'nop'));
+
+    while (!newRanInstructionsIndices.includes(nextInstructionIndex) && nextInstructionIndex < instructions.length) {
+      newRanInstructionsIndices.push(nextInstructionIndex);
+
+      const instruction = newInstructions[nextInstructionIndex];
+      const [operation, argument] = instruction.split(' ');
+
+      operation === 'acc' && (acc += +argument) && (nextInstructionIndex += 1);
+      operation === 'jmp' && (nextInstructionIndex += +argument);
+      operation === 'nop' && (nextInstructionIndex += 1);
+
+    }
+  }
+
+  console.log('Solution 2: ', acc);
+};
+
+solution();
