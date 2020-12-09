19b8768c18edb3641d8e3be034431d7d7612f22c
Ben Merken <ben.merken@gmail.com>
2020-12-09 13:28:59 +0100

    Finish day 9, part 2

--- day9/solution.js
+++ day9/solution.js
@@ -11,17 +11,40 @@ const numbers = fs
 const solution1 = () => {
   let descrepancy = 0;
 
-    numbers.slice(25).forEach((number, index) => {
-      const numbersBefore = numbers.slice(index, index + 25);
-      const sumsOtherHalves = [];
-      numbersBefore.forEach((numberBefore) => {
-        numbersBefore.includes(number - numberBefore) &&
-          sumsOtherHalves.push(numberBefore);
-      });
-      (sumsOtherHalves.length === 0) && (descrepancy = number);
+  numbers.slice(25).forEach((number, index) => {
+    const numbersBefore = numbers.slice(index, index + 25);
+    const sumsOtherHalves = [];
+    numbersBefore.forEach((numberBefore) => {
+      numbersBefore.includes(number - numberBefore) &&
+        sumsOtherHalves.push(numberBefore);
     });
+    sumsOtherHalves.length === 0 && (descrepancy = number);
+  });
 
   console.log('Solution 1: ', descrepancy);
+
+  return descrepancy;
 };
 
-solution1();
+const sortAlg = (a, b) => a > b ? 1 : -1;
+
+const solution = () => {
+  let descrepancy = solution1();
+  let contigiousNumbers = [];
+  let min, max;
+
+  numbers.forEach((number, index) => {
+    contigiousNumbers = [];
+
+    number !== descrepancy && numbers.slice(index).forEach((num) => {
+      contigiousNumbers.push(num);
+      contigiousNumbers.reduce((acc, curr) => acc + curr) === descrepancy &&
+        (min = contigiousNumbers.sort(sortAlg)[0]) &&
+        (max = contigiousNumbers.sort(sortAlg)[contigiousNumbers.length - 1]);
+    });
+  });
+
+  console.log('Solution 2: ', min + max);
+};
+
+solution();
