781c1743bbe38020cc61dc8b65ce8d6faea8fda5
Ben Merken <ben.merken@gmail.com>
2020-12-10 10:28:24 +0100

    Refactor day 10

--- day10/solution.js
+++ day10/solution.js
@@ -5,14 +5,23 @@ const path = require('path');
 
 const joltages = fs.readFileSync(path.resolve(__dirname, 'puzzle.txt'), 'utf-8').split('\r\n').map(joltage => +joltage);
 
-const solution1 = () => {
-    const joltagesSorted = joltages.sort((a, b) => a > b ? 1 : -1);
-    const joltageDifferences = [1];
+const sortAlg = (a, b) => a > b ? 1 : -1; 
 
+const getJoltageDifferences = () => {
+    const joltagesSorted = [0];
+    const joltageDifferences = [];
+
+    joltages.sort(sortAlg).map(joltage => +joltage).forEach(joltage => joltagesSorted.push(joltage));
+    joltagesSorted.push(joltagesSorted[joltagesSorted.length - 1] + 3);
     joltagesSorted.slice(1).forEach((joltage, index) => {
       joltageDifferences.push(joltage - joltagesSorted[index]);
     });
-    joltageDifferences.push(3);
+
+    return joltageDifferences;
+};
+
+const solution1 = () => {
+    const joltageDifferences = getJoltageDifferences();
 
     const solution = joltageDifferences.filter(joltage => joltage === 1).length * joltageDifferences.filter(joltage => joltage === 3).length;
 
