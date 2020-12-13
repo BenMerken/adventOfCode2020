d6c1bcfc7776118c27aa2a181659b79ac6d80f1e
Ben Merken <ben.merken@gmail.com>
2020-12-13 01:14:19 +0100

    Finish day 11

--- day11/solution.js
+++ day11/solution.js
@@ -12,8 +12,8 @@ seats.unshift('.'.repeat(seats[0].length));
 seats.push('.'.repeat(seats[0].length));
 seats = seats.map((seatsRow) => seatsRow.split(''));
 
-const getNewSeatMap = (oldSeatMap) => {
-  const newSeatMap = oldSeatMap.map(seatsRow => seatsRow.map(seat => seat));
+const getNewSeatMap1 = (oldSeatMap) => {
+  const newSeatMap = oldSeatMap.map((seatsRow) => seatsRow.map((seat) => seat));
   oldSeatMap.slice(1, oldSeatMap.length - 1).forEach((seatsRow, rowIndex) =>
     seatsRow.slice(1, seatsRow.length - 1).forEach((seat, colIndex) => {
       const adjacentSeats = [];
@@ -41,15 +41,108 @@ const getNewSeatMap = (oldSeatMap) => {
   return newSeatMap;
 };
 
+const getNewSeatMap2 = (oldSeatMap) => {
+  const newSeatMap = oldSeatMap.map((seatsRow) => seatsRow.map((seat) => seat));
+  oldSeatMap.forEach((seatsRow, rowIndex) =>
+    seatsRow.forEach((seat, colIndex) => {
+      const adjacentSeats = {
+        top: [colIndex, Number.MIN_VALUE, '.'],
+        topRight: [Number.MAX_VALUE, Number.MIN_VALUE, '.'],
+        right: [Number.MAX_VALUE, rowIndex, '.'],
+        downRight: [Number.MAX_VALUE, Number.MAX_VALUE, '.'],
+        down: [colIndex, Number.MAX_VALUE, '.'],
+        downleft: [Number.MIN_VALUE, Number.MAX_VALUE, '.'],
+        left: [Number.MIN_VALUE, rowIndex, '.'],
+        topLeft: [Number.MIN_VALUE, Number.MIN_VALUE, '.'],
+      };
+
+      oldSeatMap.forEach((seatsRowCoord, rowIndexCoord) =>
+        seatsRowCoord.forEach((seatCoord, colIndexCoord) => {
+          if (
+            (rowIndex !== rowIndexCoord ||
+            colIndex !== colIndexCoord) && seatCoord !== '.' && seat !== '.'
+          ) {
+            //diagonals
+            rowIndexCoord - rowIndex === colIndexCoord - colIndex &&
+              colIndex > colIndexCoord &&
+              adjacentSeats.topLeft[0] < colIndexCoord &&
+              (adjacentSeats.topLeft = [
+                colIndexCoord,
+                rowIndexCoord,
+                seatCoord,
+              ]);
+            rowIndexCoord - rowIndex === colIndexCoord - colIndex &&
+              colIndex < colIndexCoord &&
+              adjacentSeats.downRight[0] > colIndexCoord &&
+              (adjacentSeats.downRight = [
+                colIndexCoord,
+                rowIndexCoord,
+                seatCoord,
+              ]);
+            rowIndexCoord - rowIndex === -(colIndexCoord - colIndex) &&
+              colIndex > colIndexCoord &&
+              adjacentSeats.downleft[0] < colIndexCoord &&
+              (adjacentSeats.downleft = [
+                colIndexCoord,
+                rowIndexCoord,
+                seatCoord,
+              ]);
+            rowIndexCoord - rowIndex === -(colIndexCoord - colIndex) &&
+              colIndex < colIndexCoord &&
+              adjacentSeats.topRight[0] > colIndexCoord &&
+              (adjacentSeats.topRight = [
+                colIndexCoord,
+                rowIndexCoord,
+                seatCoord,
+              ]);
+
+            // straights
+            rowIndexCoord === rowIndex &&
+              colIndex > colIndexCoord &&
+              adjacentSeats.left[0] < colIndexCoord &&
+              (adjacentSeats.left = [colIndexCoord, rowIndexCoord, seatCoord]);
+            rowIndexCoord === rowIndex &&
+              colIndex < colIndexCoord &&
+              adjacentSeats.right[0] > colIndexCoord &&
+              (adjacentSeats.right = [colIndexCoord, rowIndexCoord, seatCoord]);
+            colIndexCoord === colIndex &&
+              rowIndex < rowIndexCoord &&
+              adjacentSeats.down[1] > rowIndexCoord &&
+              (adjacentSeats.down = [colIndexCoord, rowIndexCoord, seatCoord]);
+            colIndexCoord === colIndex &&
+              rowIndex > rowIndexCoord &&
+              adjacentSeats.top[1] < rowIndexCoord &&
+              (adjacentSeats.top = [colIndexCoord, rowIndexCoord, seatCoord]);
+          }
+        })
+      );
+      const adjacentSeatsArr = Object.values(adjacentSeats).map(
+        (adjacentSeat) => adjacentSeat[2]
+      );
+
+      const occupiedAdjecentSeats = adjacentSeatsArr.join('').match(/#/g);
+      seat === 'L' &&
+        adjacentSeatsArr.indexOf('#') === -1 &&
+        (newSeatMap[rowIndex][colIndex] = '#');
+      seat === '#' &&
+        occupiedAdjecentSeats &&
+        occupiedAdjecentSeats.length >= 5 &&
+        (newSeatMap[rowIndex][colIndex] = 'L');
+    })
+  );
+
+  return newSeatMap;
+};
+
 const solution1 = () => {
-  let seatsMap = seats.map(seatsRow => seatsRow.map(seat => seat));
-  let oldSeatsMap = seatsMap.map(seatsRow => seatsRow.map(seat => seat));
+  let seatsMap = seats.map((seatsRow) => seatsRow.map((seat) => seat));
+  let oldSeatsMap = seatsMap.map((seatsRow) => seatsRow.map((seat) => seat));
   while (
-    (seatsMap = getNewSeatMap(seatsMap))
+    (seatsMap = getNewSeatMap1(seatsMap))
       .map((seatsRow) => seatsRow.join(''))
       .join('') !== oldSeatsMap.map((seatsRow) => seatsRow.join('')).join('')
   ) {
-    oldSeatsMap = seatsMap.map(seatsRow => seatsRow.map(seat => seat));
+    oldSeatsMap = seatsMap.map((seatsRow) => seatsRow.map((seat) => seat));
   }
 
   console.log(
@@ -61,4 +154,29 @@ const solution1 = () => {
   );
 };
 
+const solution2 = () => {
+  let seatsMap = seats.map((seatsRow) => seatsRow.map((seat) => seat));
+  let oldSeatsMap = seatsMap.map((seatsRow) => seatsRow.map((seat) => seat));
+  while (
+    (seatsMap = getNewSeatMap2(seatsMap))
+      .map((seatsRow) => seatsRow.join(''))
+      .join('') !== oldSeatsMap.map((seatsRow) => seatsRow.join('')).join('')
+  ) {
+    oldSeatsMap = seatsMap.map((seatsRow) => seatsRow.map((seat) => seat));
+  }
+
+  console.log(
+    'Solution 2: ',
+    seatsMap
+      .map((seatsRow) => seatsRow.join(''))
+      .join('')
+      .match(/#/g).length
+  );
+
+  console.log(seatsMap
+    .map((seatsRow) => seatsRow.join('') + '\r\n')
+    .join(''))
+};
+
 solution1();
+solution2();
