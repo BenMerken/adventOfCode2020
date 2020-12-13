3fe788f72fcec7668778f2f9e0ba644045f4086a
Ben Merken <ben.merken@gmail.com>
2020-12-13 20:20:13 +0100

    Finish day 12

--- day12/solution.js
+++ day12/solution.js
@@ -5,26 +5,6 @@ const path = require('path');
 
 const instructions = fs.readFileSync(path.resolve(__dirname, 'puzzle.txt'), 'utf-8').split('\r\n');
 
-const changeFacingLeft = (currentFacing) => {
-  let newFacing;
-  currentFacing === 'N' && (newFacing = 'W');
-  currentFacing === 'E' && (newFacing = 'N');
-  currentFacing === 'S' && (newFacing = 'E');
-  currentFacing === 'W' && (newFacing = 'S');
-
-  return newFacing;
-};
-
-const changeFacingRight = (currentFacing) => {
-    let newFacing;
-    currentFacing === 'N' && (newFacing = 'E');
-    currentFacing === 'E' && (newFacing = 'S');
-    currentFacing === 'S' && (newFacing = 'W');
-    currentFacing === 'W' && (newFacing = 'N');
-
-    return newFacing;
-};
-
 const solution1 = () => {
     let currentFacing = 'E';
     const stepsInDirections = {
@@ -40,7 +20,26 @@ const solution1 = () => {
         currentFacing === 'S' && (stepsInDirections.south += steps);
         currentFacing === 'W' && (stepsInDirections.west  += steps);
     };
-    
+
+    const changeFacingLeft = () => {
+        let newFacing;
+        currentFacing === 'N' && (newFacing = 'W');
+        currentFacing === 'E' && (newFacing = 'N');
+        currentFacing === 'S' && (newFacing = 'E');
+        currentFacing === 'W' && (newFacing = 'S');
+      
+        return newFacing;
+      };
+      
+      const changeFacingRight = () => {
+          let newFacing;
+          currentFacing === 'N' && (newFacing = 'E');
+          currentFacing === 'E' && (newFacing = 'S');
+          currentFacing === 'S' && (newFacing = 'W');
+          currentFacing === 'W' && (newFacing = 'N');
+      
+          return newFacing;
+      };            
 
     instructions.forEach(instruction => {
       const [direction, steps] = [instruction.substring(0,1), +instruction.substring(1)];
@@ -49,8 +48,8 @@ const solution1 = () => {
       direction === 'E' && (stepsInDirections.east += steps);
       direction === 'S' && (stepsInDirections.south += steps);
       direction === 'W' && (stepsInDirections.west += steps);
-      direction === 'L' && (Array(steps  / 90).fill().forEach(() => currentFacing = changeFacingLeft(currentFacing)));
-      direction === 'R' && (Array(steps / 90).fill().forEach(() => currentFacing = changeFacingRight(currentFacing)));
+      direction === 'L' && (Array(steps  / 90).fill().forEach(() => currentFacing = changeFacingLeft()));
+      direction === 'R' && (Array(steps / 90).fill().forEach(() => currentFacing = changeFacingRight()));
       direction === 'F' && (sailForward(steps))
     });
 
@@ -62,4 +61,75 @@ const solution1 = () => {
      console.log('Solution 1: ', northSouth + eastWest)
 };
 
+const solution2 = () => {
+    const shipPosition = {N: 0, E: 0, S: 0, W: 0};
+    let waypointRelativePosition = [[10, 'E'], [1, 'N']];
+
+    const turnWayPointLeft = () => {
+        switch (waypointRelativePosition[0][1]) {
+            case 'N':
+                (waypointRelativePosition[0][1] = 'W') && (waypointRelativePosition[1][1] = 'S');
+                break;
+            case 'E':
+                (waypointRelativePosition[0][1] = 'N') && (waypointRelativePosition[1][1] = 'W');
+                break;
+            case 'S':
+                (waypointRelativePosition[0][1] = 'E') && (waypointRelativePosition[1][1] = 'N');
+                break;
+            case 'W':
+                (waypointRelativePosition[0][1] = 'S') && (waypointRelativePosition[1][1] = 'E');
+                break;
+        }
+    };
+
+    const turnWaypointRight = () => {
+        switch (waypointRelativePosition[0][1]) {
+            case 'N':
+                (waypointRelativePosition[0][1] = 'E') && (waypointRelativePosition[1][1] = 'N');
+                break;
+            case 'E':
+                (waypointRelativePosition[0][1] = 'S') && (waypointRelativePosition[1][1] = 'E');
+                break;
+            case 'S':
+                (waypointRelativePosition[0][1] = 'W') && (waypointRelativePosition[1][1] = 'S');
+                break;
+                case 'W':
+                (waypointRelativePosition[0][1] = 'N') && (waypointRelativePosition[1][1] = 'W');
+                break;
+        }
+    };
+
+    const moveWaypoint = (direction, steps) => {
+        let inverse;
+        direction === 'N' && (inverse = 'S');
+        direction === 'E' && (inverse = 'W');
+        direction === 'S' && (inverse = 'N');
+        direction === 'W' && (inverse = 'E');
+
+        waypointRelativePosition.forEach(position => position.indexOf(direction) > -1 && (position[0] += steps));
+        waypointRelativePosition.forEach(position => position.indexOf(inverse) > -1 && (position[0] -= steps));
+    };
+
+    const sailForward = (multiplier) => {
+        waypointRelativePosition.forEach(position => shipPosition[`${position[1]}`] += position[0] * multiplier);
+    };
+
+    instructions.forEach(instruction => {
+        const [direction, steps] = [instruction.substring(0, 1), +instruction.substring(1)];
+
+        direction.match(/(N|E|S|W)/) && (moveWaypoint(direction, steps));
+        direction === 'L' && (Array(steps  / 90).fill().forEach(() => turnWayPointLeft()));
+        direction === 'R' && (Array(steps / 90).fill().forEach(() => turnWaypointRight()));
+        direction === 'F' && (sailForward(steps));
+    });
+
+    const northSouth = shipPosition.N > shipPosition.S
+     ? shipPosition.N - shipPosition.S : shipPosition.S - shipPosition.N;
+     const eastWest = shipPosition.E > shipPosition.W
+     ? shipPosition.E - shipPosition.W : shipPosition.W - shipPosition.E;
+
+    console.log('Solution 2: ', northSouth + eastWest);
+};
+
 solution1();
+solution2();
