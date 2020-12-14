8802a4af40c44a558f1125414be30bbc285a8024
Ben Merken <ben.merken@gmail.com>
2020-12-14 02:12:37 +0100

    Finish day 13?
    Very, VERY slow way of doing thigs. I don't even know whether it's good
    or not, since my computer is still crunching the numbers, but I've
    tested the five/six examples on the website, and they all worked.
    
    I've read some stuff on the subreddit about the so-called "Chinese
    Remainder Theorem", but I don't like to include stuff into my solutions
    I haven't come up with myself.

--- day13/solution.js
+++ day13/solution.js
@@ -3,23 +3,42 @@
 const fs = require('fs');
 const path = require('path');
 
-let [timestamp, buses] = fs.readFileSync(path.resolve(__dirname, 'puzzle.txt'), 'utf-8').split('\r\n');
+let [timestamp, buses] = fs
+  .readFileSync(path.resolve(__dirname, 'puzzle.txt'), 'utf-8')
+  .split('\r\n');
 timestamp = +timestamp;
-buses = buses.split(',').filter(bus => bus !== 'x').map(bus => +bus);
+buses = buses.split(',');
 
 const solution1 = () => {
-    const busesAndMinutes = [];
-    buses.forEach(busId => {
-        let minutesPassed = 0;
+  const actualBuses = buses.filter((busId) => busId !== 'x');
+  const busesAndMinutes = [];
+  actualBuses.forEach((busId) => {
+    let minutesPassed = 0;
 
-        while ((timestamp + minutesPassed) % busId !== 0) {
-            minutesPassed++;
-        }
+    while ((timestamp + minutesPassed) % busId !== 0) {
+      minutesPassed++;
+    }
 
-        busesAndMinutes.push(busId * minutesPassed);
-    });
+    busesAndMinutes.push(busId * minutesPassed);
+  });
 
-    console.log('Solution 1: ', Math.min(...busesAndMinutes));
+  console.log('Solution 1: ', Math.min(...busesAndMinutes));
+};
+
+const solution2 = () => {
+  const actualBuses = buses.filter((busId) => busId !== 'x');
+  const minutesPlusTimestamp = {};
+  let earliestTimestamp = 100000000000000;
+  actualBuses.forEach(
+    (busId) => (minutesPlusTimestamp[busId] = buses.indexOf(busId))
+  );
+
+  while (!Object.entries(minutesPlusTimestamp).every(([key, value]) => (earliestTimestamp + value) % +key === 0)) {
+    earliestTimestamp++;
+  }
+
+  console.log('Solution 2: ', earliestTimestamp);
 };
 
 solution1();
+solution2();
